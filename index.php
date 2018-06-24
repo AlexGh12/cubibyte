5c644100290981df8fa81b122a92bfcc395e0872
Alex G'h <alexgh@la-MacBook-Air-de-Alex.local>
2018-06-24 11:54:56 -0500

    iniciamos proyecto de cubibyte

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    if ( isset($_GET['p']) ) {
+
+        switch ( $_GET['p'] ) {
+            case 'var':            
+                include('View/file.php');
+                break;
+            
+            default:
+                include('View/inicio.php');
+                break;
+        }
+        
+    }else{
+        include('View/inicio.php');
+    }
+
+?>
