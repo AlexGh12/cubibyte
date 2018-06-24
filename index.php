7a97b1bafe7a96391aee8b0e7708a96e5e8770a9
Alex G'h <alexgh@la-MacBook-Air-de-Alex.local>
2018-06-24 13:37:31 -0500

    cambiamos tabuladores

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-    if ( isset($_GET['p']) ) {
+	if ( isset($_GET['p']) ) {
 
-        switch ( $_GET['p'] ) {
-            case 'var':            
-                include('View/file.php');
-                break;
-            
-            default:
-                include('View/inicio.php');
-                break;
-        }
-        
-    }else{
-        include('View/inicio.php');
-    }
+		switch ( $_GET['p'] ) {
+			case 'var':            
+				include('View/file.php');
+				break;
+			
+			default:
+				include('View/inicio.php');
+				break;
+		}
+		
+	}else{
+		include('View/inicio.php');
+	}
 
 ?>
